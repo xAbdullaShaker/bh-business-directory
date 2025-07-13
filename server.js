738caf003517133a4cd9b666b309033a7394401e
Nabila Ayaba <nabilaayaba@gmail.com>
2025-07-13 16:02:28 +0300

    Build landing page

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, 'public')))
 
 // GET / (home)
 app.get('/', (req, res) => {
-    res.send('hello, friend')
+    res.render('index.ejs')
 })
 
 app.listen(3000, () => {
