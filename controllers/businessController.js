87bac6a78645b212c9cf63d06e9eb2954c0ab7a2
xabdulla <abdullashaker002@gmail.com>
2025-07-14 13:10:49 +0300

    updated

---
+++ controllers/businessController.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const Business = require('../models/business')
+
+// TEST ROUTE
+router.get('/', (req, res) => {
+    res.send('do i work?')
+})
+
+// RENDER NEW BUSINESS FORM
+router.get('/new', (req, res) => {
+    res.render('businesses/new.ejs')
+})
+
+// POST FORM DATA TO DATABASE
+router.post('/', async (req, res) => {
+    if (req.body.isVerified === 'on') {
+        req.body.isVerified = true
+    } else {
+        req.body.isVerified = false
+    }
+    console.log(req.body)
+    await Business.create(req.body)
+    res.redirect('/businesses/new')
+})
+
+
+module.exports = router
