2a098188dd60c6c0c60b55d07366a40505aca62d
Nabila Ayaba <nabilaayaba@gmail.com>
2025-07-14 09:25:34 +0300

    Connect to database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
+
+// DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, 'public')))
